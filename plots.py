cc4691b3e10b2c774b174c1e3db45d26c795491c
leohallier <leonard.hallier@gmail.com>
2022-10-16 20:02:22 +0200

    Created a plot that is surprisingly helpful and promising (was mostly trying things out). Seems to show predictive power of V1 variable.

--- plots.py
+++ plots.py
@@ -1,4 +1,15 @@
-import pandas
+import pandas as pd
+import matplotlib.pyplot as plt
 
-df = pandas.read_csv("creditcard.csv")
+df = pd.read_csv("creditcard.csv")
 
+grouped_df = df.groupby("Class")
+frauds_df = grouped_df.get_group(1)
+
+samples_df = grouped_df.get_group(0).sample(1000)
+
+combined_df = pd.concat([samples_df, frauds_df])
+
+combined_df.plot(x="V1", y="Class", kind="scatter")
+
+plt.show()
